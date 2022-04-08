4173f0388b0983dc5f0f0be4a2d378e084af3781
nimsandu <57834844+nimsandu@users.noreply.github.com>
2022-04-08 13:18:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 set -e
 
 echo "Beginning installation of spicetify-fluent"
-echo "https://github.com/nimsandu/Fluent-Spicetify"
+echo "https://github.com/nimsandu/Bloom-Spicetify"
 
 # Give time for user to cancel via CTRL+C
 sleep 3s
 
 # Check if ~\.spicetify-cli\Themes\Fluent directory exists
 spicePath=dirname "$(spicetify -c)"
-themePath=dirname "$spicePath/Themes/Fluent"
+themePath=dirname "$spicePath/Themes/Bloom"
 if [ ! -d $themePath ]; then
-    echo "Creating Fluent theme folder..."
+    echo "Creating Bloom theme folder..."
     mkdir -p $themePath
 else
     # Remove pre-existing files, only keep the newest files
@@ -20,17 +20,17 @@ else
 fi
 
 # Download latest master
-zipUri="https://github.com/nimsandu/Fluent-Spicetify/archive/refs/heads/master.zip"
-zipSavePath="$themePath/fluent-master.zip"
-echo "Downloading spicetify-fluent latest master..."
+zipUri="https://github.com/nimsandu/Bloom-Spicetify/archive/refs/heads/master.zip"
+zipSavePath="$themePath/Bloom-master.zip"
+echo "Downloading bloom-spicetify latest master..."
 curl --fail --location --progress-bar --output "$zipUri" "$zipSavePath"
 
 
 # Extract theme from .zip file
 echo "Extracting..."
 unzip -d "$themePath" -o "$zipSavePath"
-mv "$themePath/spicetify-fluent-master/*" $themePath
-rmdir "$themePath/spicetify-fluent-master"
+mv "$themePath/Bloom-spicetify-master/*" $themePath
+rmdir "$themePath/Bloom-spicetify-master"
 
 # Delete .zip file
 echo "Deleting zip file..."
@@ -42,14 +42,14 @@ cd $themePath
 # Copy the fluent.js to the Extensions folder
 mkdir -p ../../Extensions
 cp fluent.js ../../Extensions/.
-echo "+ Installed fluent.js extension"
+echo "+ Installed bloom.js extension"
 
 # Apply the theme with spicetify config calls
-spicetify config extensions fluent.js
-spicetify config current_theme Fluent
+spicetify config extensions bloom.js
+spicetify config current_theme Bloom
 spicetify config color_scheme dark
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
-echo "+ Configured Fluent theme"
+echo "+ Configured Bloom theme"
 
 # Patch the xpui.js for sidebar fixes
 # credit: https://github.com/JulienMaille/dribbblish-dynamic-theme/blob/main/install.sh
