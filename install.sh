e2324a96232b206053f857ed2d611a0ba214ba55
javalsai <jvssxxi@gmail.com>
2022-07-08 18:02:32 +0200

    Fix `install.sh` script

--- install.sh
+++ install.sh
@@ -8,40 +8,31 @@ echo "https://github.com/nimsandu/spicetify-bloom"
 # Give time for user to cancel via CTRL+C
 sleep 3s
 
-# Check if ~\.spicetify-cli\Themes\bloom directory exists
-spicePath=dirname "$(spicetify -c)"
-themePath=dirname "$spicePath/Themes/bloom"
-if [ ! -d $themePath ]; then
-    echo "Creating bloom theme folder..."
-    mkdir -p $themePath
-else
-    # Remove pre-existing files, only keep the newest files
-    rm -rfv "$themePath/*"
+# Check if $spicePath\Themes\bloom directory exists
+spicePath="$(dirname "$(spicetify -c)")"
+themePath="$spicePath/Themes/bloom"
+if [ -d "$themePath" ]; then
+    rm -rf "$themePath"
 fi
 
 # Download latest master
 zipUri="https://github.com/nimsandu/spicetify-bloom/archive/refs/heads/master.zip"
-zipSavePath="$themePath/bloom-main.zip"
+zipSavePath="/tmp/bloom-main.zip"
 echo "Downloading bloom-spicetify latest master..."
-curl --fail --location --progress-bar --output "$zipUri" "$zipSavePath"
-
+curl --fail --location --progress-bar "$zipUri" --output "$zipSavePath"
 
 # Extract theme from .zip file
 echo "Extracting..."
-unzip -d "$themePath" -o "$zipSavePath"
-mv "$themePath/spicetify-bloom-main/*" $themePath
-rmdir "$themePath/spicetify-bloom-main"
+unzip -q "$zipSavePath" -d "$spicePath"
+mv "$spicePath/spicetify-bloom-main/" "$themePath"
 
 # Delete .zip file
 echo "Deleting zip file..."
 rm "$zipSavePath"
 
-# Change Directory to the Theme Folder
-cd $themePath
-
 # Copy the fluent.js to the Extensions folder
-mkdir -p ../../Extensions
-cp fluent.js ../../Extensions/.
+mkdir -p "$spicePath/Extensions"
+cp "$themePath/bloom.js" "$spicePath/Extensions"
 echo "+ Installed bloom.js extension"
 
 # Apply the theme with spicetify config calls
@@ -57,9 +48,9 @@ PATCH='[Patch]
 xpui.js_find_8008 = ,(\\w+=)32,
 xpui.js_repl_8008 = ,\${1}58,'
 if cat config-xpui.ini | grep -o '\[Patch\]'; then
-    perl -i -0777 -pe "s/\[Patch\].*?($|(\r*\n){2})/$PATCH\n\n/s" config-xpui.ini
+    perl -i -0777 -pe "s/\[Patch\].*?($|(\r*\n){2})/$PATCH\n\n/s" "$spicePath/config-xpui.ini"
 else
-    echo -e "\n$PATCH" >> config-xpui.ini
+    echo -e "\n$PATCH" >> "$spicePath/config-xpui.ini"
 fi
 echo "+ Patched xpui.js for Sidebar fixes"
 
