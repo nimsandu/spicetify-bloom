f40b1d3f8af89ae868762c24f9e963f46488abcd
javalsai <jvssxxi@gmail.com>
2022-07-12 19:10:04 +0200

    Fix patching

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ echo "+ Configured Bloom theme"
 PATCH='[Patch]
 xpui.js_find_8008 = ,(\\w+=)32,
 xpui.js_repl_8008 = ,\${1}58,'
-if cat config-xpui.ini | grep -o '\[Patch\]'; then
+if cat "$spicePath/config-xpui.ini" | grep -o '\[Patch\]'; then
     perl -i -0777 -pe "s/\[Patch\].*?($|(\r*\n){2})/$PATCH\n\n/s" "$spicePath/config-xpui.ini"
 else
     echo -e "\n$PATCH" >> "$spicePath/config-xpui.ini"
