nimsandu/spicetify-bloom

commit f6e62f8bd58155c50b9a5ba4a0cc1498a5b05149

install(sh): Try to recover from applying process (#123)
* This implementation is rather hacky but solid. It does work though.

Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

install.sh
@@ -66,5 +66,9 @@ else
 fi
 echo "+ Patched xpui.js for Sidebar fixes"
 
-spicetify apply
+spicetify apply \
+|| echo "Spicetify failed! Attempt to recover!" \
+&& spicetify backup apply \
+|| echo "Still failing! See output for more info!" \
+&& return 1
 echo "+ Applied Theme"

commit a4325b98aba3c5dceda76ae54d1dcd821006e8ca

install(sh): Approach recovery through use of functions (#126)
Fixes: f6e62f8bd58155c50b9a5ba4a0cc1498a5b05149
Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

install.sh
@@ -68,9 +68,19 @@ else
 fi
 echo "+ Patched xpui.js for Sidebar fixes"
 
-spicetify apply \
-|| echo "Spicetify failed! Attempt to recover!" \
-&& spicetify backup apply \
-|| echo "Still failing! See output for more info!" \
-&& return 1
+# We need to do function approach instead apparently.
+function recover_from_failure() {
+    echo "? Spicetify failed! Attempt to recover!"
+    spicetify backup apply
+    if [ $? != 0 ]; then
+        echo "! Still failing! See output for more info!"
+        # b/126
+        exit 2
+    fi
+}
+
+spicetify apply
+if [ $? != 0 ]; then
+    recover_from_failure
+fi
 echo "+ Applied Theme"