d7887c68eae935e0d010b170f986c56a486dd72a
Beru Hinode <windowz414@1337.lgbt>
2022-11-04 17:37:31 +0300

    Initial script for Debian (#125)
    * Also added a strong safeguard to abort if the distro isn't
      Debian or its derivative.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

---
+++ install_debian.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/bash
+
+#
+# Bloom theme for Debian and its derivatives
+#
+# Made on Pardus with <3 by Beru "Stella" Hinode
+#
+# Copyright (C) Beru "Stella" Hinode (for Nimsandu Kottage)
+#
+# SPDX-License-Identifier: MIT
+#
+
+# Preliminary checks
+echo "Checking your distribution as a safeguard..."
+DISTRO_REL="$(grep 'ID' /etc/os-release | sed 's/.*=//')"
+# TODO: We really need to maintain a list for this and use it instead.
+if [ -z "$DISTRO_REL" ]; then
+    echo "Your distrobution could not be determined."
+    echo "Please check if you have /etc/os-release present."
+    echo "If not, please see if there's a way to determine your distribution and open an issue."
+    echo "This script is strictly for Debian and its derivatives, excluding Ubuntu and its derivatives."
+    echo "If your distribution doesn't fall in this category this script was made for, please use generic installation instead."
+    echo -e "\nAbort!"
+    exit 2
+elif [ "$DISTRO_REL" == "ubuntu" ]; then
+    echo "Your distribution is Ubuntu."
+    echo -e "Distro author: Canonical\n"
+    echo "Ubuntu doesn't have any derps of Spotify."
+    echo "Please use generic installation method instead, it would work on it."
+    echo -e "\nAbort!"
+    exit 1
+elif [ "$DISTRO_REL" == "pardus" ]; then
+    echo "Your distribution is Pardus, which is a derivative of Debian. Continue."
+    echo -e "Distro author: TÜBİTAK ULAKBİLİM\n"
+    continue
+elif [ "$DISTRO_REL" != "debian" ]; then
+    echo "Your distribution doesn't seem to be Debian or its derivative."
+    echo "If this is a mistake, please open a GitHub issue with the ID detected: $DISTRO_REL"
+    echo -e "\nAbort!"
+    exit -1
+else
+    echo "Your distribution is Debian. Continue."
+    echo -e "Distro author: Software in the Public Interest, Inc.\n"
+fi
+
+# First, let's execute the main script as things are practically the
+# same.
+echo -e "Getting main script and running it...\n"
+curl -fsSL https://raw.githubusercontent.com/nimsandu/spicetify-bloom/main/install.sh | bash
+
+# Let's see if an abortion occurred. If so, abort with it.
+if [ $? != 0 ]; then
+    exit $?
+fi
+
+# Now comes derp fixup.
+echo -e "\nDebian derp! Spotify won't launch without this fix!"
+echo "See: https://community.spotify.com/t5/Desktop-Linux/The-app-crashes-on-Debian-due-to-HW-acceleration/td-p/5049188"
+
+# User might have already done the fix before on something else.
+# Let's see if they really did.
+FIXCHECK="$(grep '^Exec=' /usr/local/share/applications/spotify.desktop | grep '\-\-no-zygote')"
+if [ ! -z "$FIXCHECK" ]; then
+    # The user DID apply the fix, nothing to do.
+    echo -e "\nYou already did apply the fix. Success!"
+    exit 0
+fi
+
+echo "Now applying the fix suggested..."
+# We need root access here because we can't write in /usr as regular
+# user.
+if [ "$USER" != "root" ]; then
+    echo "Root access needed!"
+    echo "Please enter your password so we can apply the fix properly!"
+    # Doing exit here is safe, because it runs the command within
+    # another shell, not on the shell this script is run within.
+    sudo bash -c "exit"
+fi
+
+# We can do sudo even as root, no worries on that.
+sudo sed -i '/^Exec=/ s/spotify/spotify --no-zygote/' /usr/local/share/applications/spotify.desktop
