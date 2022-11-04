nimsandu/spicetify-bloom

commit 7d21013d5af93b9e8fae745d9dbf16a272f49cf5

install(sh): Add preliminary check for Spicetify's existence (#119)
Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

install.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ -z "$(which spicetify)" ]; then
+    echo "Spicetify isn't installed or doesn't exist in your PATH."
+    echo "Bloom relies on it to work properly."
+    echo "Please install Spicetify (https://spicetify.app) and run this script again to continue."
+    echo "If Spicetify is already installed, add it to your PATH variable and rerun this script."
+    echo "Example command for adding to PATH: export PATH=/opt/spicetify:\$PATH"
+    echo "\nAbort!"
+    return 1
+fi
+
 echo "Beginning installation of spicetify-bloom"
 echo "https://github.com/nimsandu/spicetify-bloom"
 

commit b8f872e045fa6ef1d660122aa6ed40b1df4e008c

install(sh): Approach with `exit` instead (#126)
Fixes: 7d21013d5af93b9e8fae745d9dbf16a272f49cf5
Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

install.sh
@@ -9,7 +9,9 @@ if [ -z "$(which spicetify)" ]; then
     echo "If Spicetify is already installed, add it to your PATH variable and rerun this script."
     echo "Example command for adding to PATH: export PATH=/opt/spicetify:\$PATH"
     echo "\nAbort!"
-    return 1
+    # Exit approach seems to work better.
+    # b/126
+    exit 1
 fi
 
 echo "Beginning installation of spicetify-bloom"