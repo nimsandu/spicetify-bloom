352ff42ec331c86ca1c636e8c37546e975aebb13
Miukyo <zaidaliyhijja@gmail.com>
2022-12-13 17:47:57 +0700

    Update using specific parent selector instead

--- bloom.js
+++ bloom.js
@@ -91,14 +91,14 @@
     var playButtonStyle = document.createElement('style');
     playButtonStyle.innerHTML = `
       .main-playButton-button[aria-label="${Spicetify.Platform.Translations.play}"],
-      .main-playButton-PlayButton button[aria-label~="${Spicetify.Platform.Translations.play}"],
+      .main-playButton-PlayButton>button[aria-label~="${Spicetify.Platform.Translations.play}"],
       .main-playPauseButton-button[aria-label="${Spicetify.Platform.Translations.play}"],
       .main-trackList-rowPlayPauseButton[aria-label="${Spicetify.Platform.Translations.play}"] {
         background-color: var(--spice-text) !important;
         -webkit-mask-image: url('https://cdn.jsdelivr.net/gh/nimsandu/spicetify-bloom@master/assets/fluentui-system-icons/ic_fluent_play_24_filled.svg') !important;
       }
       .main-playButton-button[aria-label="${Spicetify.Platform.Translations.pause}"],
-      .main-playButton-PlayButton button[aria-label~="${Spicetify.Platform.Translations.pause}"],
+      .main-playButton-PlayButton>button[aria-label~="${Spicetify.Platform.Translations.pause}"],
       .main-playPauseButton-button[aria-label="${Spicetify.Platform.Translations.pause}"],
       .main-trackList-rowPlayPauseButton[aria-label="${Spicetify.Platform.Translations.pause}"] {
         background-color: var(--spice-text) !important;
