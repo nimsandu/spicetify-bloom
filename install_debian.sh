5b4c4349b242e6e8ff444f49c575454b772afb54
Beru Hinode <windowz414@1337.lgbt>
2022-12-30 10:33:03 +0300

    install_debian: Add Linux Mint
    * It's also derped.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- install_debian.sh
+++ install_debian.sh
@@ -40,6 +40,10 @@ elif [ "$DISTRO_REL" == "pardus" ]; then
     echo "Your distribution is Pardus, which is a derivative of Debian. Continue."
     echo -e "Distro author: TÜBİTAK ULAKBİLİM\n"
     continue
+elif [ "$DISTRO_REL" == "linuxmint" ]; then
+    echo "Your distribution is Linux Mint, which is a derivative of Debian. Continue."
+    echo -e "Distro author: Linux Mark Institute\n"
+    continue
 elif [ "$DISTRO_REL" != "debian" ]; then
     echo "Your distribution doesn't seem to be Debian or its derivative."
     echo "If this is a mistake, please open a GitHub issue with the ID detected: $DISTRO_REL"
