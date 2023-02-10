73551741d2ef4e89d5e72d58b3d683bb1fbfaf29
Nimsandu Kottage <57834844+nimsandu@users.noreply.github.com>
2023-02-10 08:40:06 +0530

    Update bloom.js

--- bloom.js
+++ bloom.js
@@ -72,7 +72,7 @@ mainRootlistWrapper.style.height = (mainRootlistWrapper.offsetHeight * 2) + "px"
               size: "72px",
             };
             if (!image.url) {
-              icon.size = "44px";
+              icon.size = "30px";
               }
             cache.set(id, icon);
             break;
