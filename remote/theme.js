8f69d4c26626563983778634583a6e6881f4d052
Sunset <grigory.orlov.set@gmail.com>
2023-06-09 02:09:53 +0500

    chore(theme): migrate to remote

---
+++ remote/theme.js
@@ -0,0 +1,6 @@
+(() => {
+  const themeScript = document.createElement('script');
+  themeScript.type = 'text/javascript';
+  themeScript.src = 'https://nimsandu.github.io/spicetify-bloom/src/theme.js';
+  document.head.appendChild(themeScript);
+})();
