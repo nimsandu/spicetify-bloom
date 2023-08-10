4e21958914ac33f3b24c821bdff0b17a6108c59e
Grigory <techuila.sunset@gmail.com>
2023-08-11 02:32:43 +0500

    changes 5

--- src/script/constants.ts
+++ src/script/constants.ts
@@ -1,5 +1,5 @@
 export const githubPagesURL = "https://nimsandu.github.io/spicetify-bloom";
-export const assetsURL = `${githubPagesURL}/src/assets`;
+export const assetsURL = `${githubPagesURL}/assets`;
 export const fluentIconsURL = `${assetsURL}/fluentui-system-icons`;
 
 export const noiseOpacityVariable = "--noise-opacity";
