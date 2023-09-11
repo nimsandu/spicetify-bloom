5c4026ffe349ba4b6b78aa4a9bc8142384f9c74d
Grigory <techuila.sunset@gmail.com>
2023-09-12 00:01:04 +0500

    feat: settings

--- src/script/app.ts
+++ src/script/app.ts
@@ -1,3 +1,4 @@
+import { SettingsSection } from "spcr-settings";
 import styleCategoryCards from "./features/categoryCardBackdrops/styleCategoryCards";
 import checkRequirements from "./features/checkRequirements/checkRequirements";
 import addButtonStyles from "./main/modules/addButtonStyles";
@@ -7,6 +8,18 @@ import fixTippiesBackdropFilter from "./main/modules/fixTippiesBackdropFilter";
 import keepTopBarContentCentered from "./main/modules/keepTopBarContentCentered";
 
 async function bloom() {
+  const settings = new SettingsSection("Bloom theme settings", "bloom-settings");
+  settings.addToggle("check-requirements", "Check the theme requirements", true);
+  settings.addToggle(
+    "fix-tippies-backdrop-filter",
+    "Fix some menus and flyouts blur (might be resource instensive)",
+    true,
+    () => {
+      window.location.reload();
+    },
+  );
+  settings.pushSettings();
+
   // const textColor = getComputedStyle(document.documentElement).getPropertyValue("--spice-text");
   // if (textColor === " #000000") {
   //   document.documentElement.style.setProperty("--filter-brightness", 0);
@@ -17,8 +30,8 @@ async function bloom() {
   addLibraryNavLinkAccent();
   controlNoiseOpacity();
   styleCategoryCards();
-  fixTippiesBackdropFilter();
-  checkRequirements();
+  if (settings.getFieldValue("fix-tippies-backdrop-filter")) fixTippiesBackdropFilter();
+  if (settings.getFieldValue("check-requirements")) checkRequirements();
 }
 
 export default bloom;
