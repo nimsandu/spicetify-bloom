0f4c46411f2d0a7e997df0bf4157a89e4e05ef59
Grigory <techuila.sunset@gmail.com>
2023-09-17 23:32:28 +0500

    first style changes

--- src/script/features/categoryCardBackdrops/constants/constants.ts
+++ src/script/features/categoryCardBackdrops/constants/constants.ts
@@ -1,4 +1,3 @@
 export const categoryCardClass = "x-categoryCard-CategoryCard";
 export const categoryCardImageClass = "x-categoryCard-image";
 export const categoryCardBackdropClass = "x-categoryCard-backdrop";
-export const categoryCardBodyClass = "style-category-cards";
--- src/script/features/categoryCardBackdrops/styleCategoryCards.ts
+++ src/script/features/categoryCardBackdrops/styleCategoryCards.ts
@@ -1,11 +1,8 @@
 import waitForAPIs from "../../shared/utils/waitForAPIs";
-import { categoryCardBodyClass } from "./constants/constants";
 import addBackdropToCategoryCards from "./modules/addBackdropToCategoryCards";
 
 function styleCategoryCards(): void {
   waitForAPIs(["Spicetify.Platform.History"], () => {
-    document.body.classList.add(categoryCardBodyClass);
-
     const { History } = Spicetify.Platform;
 
     function applyStyle(): void {
