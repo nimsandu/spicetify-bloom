d5016c268f1294995971063f5b2bce6b614f4973
Grigory <techuila.sunset@gmail.com>
2023-09-19 23:33:25 +0500

    update bloomLyricsStyleSettingTitle

--- src/script/main/constants/constants.ts
+++ src/script/main/constants/constants.ts
@@ -18,4 +18,4 @@ export const themeSettingsSectionName = "Bloom theme settings";
 export const requirementsSettingTitle = "Check the theme requirements";
 export const tippiesBackdropSettingTitle =
   "Fix some menus and flyouts background blur (may be resource intensive - read the Troubleshooting section in the theme README for more info)";
-export const bloomLyricsStyleSettingTitle = "Enable Bloom lyrics style (AM-like)";
+export const bloomLyricsStyleSettingTitle = "Enable Bloom lyrics style (not compatible with the Beautiful Lyrics extension)";
