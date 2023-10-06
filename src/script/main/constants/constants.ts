5d839a78a8f4f95d357223545b0a41e3879e3a0e
Grigory <techuila.sunset@gmail.com>
2023-10-06 18:54:47 +0500

    hide easter egg better

--- src/script/main/constants/constants.ts
+++ src/script/main/constants/constants.ts
@@ -37,7 +37,7 @@ export const easterEggButtonID = "easter-egg";
 export const easterEggButtonDescription = "Press this button";
 export const easterEggButtonValue = "Yes, do it!";
 export const easterEggVideoURL =
-  "https://raw.githubusercontent.com/nimsandu/spicetify-bloom/dist/assets/easter-egg.webm";
+  "https://github.com/nimsandu/spicetify-bloom/assets/115353812/fdac793b-e0dd-4058-b29e-66ca2583beda";
 
 export const githubContentsAPIAssetsURL =
   "https://api.github.com/repos/nimsandu/spicetify-bloom/contents/assets?ref=dist";
